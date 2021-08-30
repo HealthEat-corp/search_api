d2c613aa59c275d6b71fb71d79353b3e2ab4c1e7
JungwooSim <cscd053@gmail.com>
2021-08-30 13:50:09 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/healtheat/search_api/SearchApiApplication.kt
@@ -0,0 +1,11 @@
+package com.healtheat.search_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SearchApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SearchApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/healtheat/search_api/SearchApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.healtheat.search_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SearchApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
